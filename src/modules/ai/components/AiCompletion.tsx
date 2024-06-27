40887032d4dc4036a888fd3c14053e4520056685
lusv <viacheslav.lushchinskiy@elca.ch>
2024-06-27 10:14:26 +0200

    scrawl done

--- src/modules/ai/components/AiCompletion.tsx
+++ src/modules/ai/components/AiCompletion.tsx
@@ -2,12 +2,13 @@
 
 import ReactMarkdown from "react-markdown";
 import { streamTextAction, updateTodoAiRec } from "@/modules/todo/todo-actions";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { readStreamableValue } from "ai/rsc";
 import { useFormStatus } from "react-dom";
 import { useTranslations } from "next-intl";
 
 export default function AiCompletion() {
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
   const [completion, setCompletion] = useState("");
   const { pending, data } = useFormStatus();
 
@@ -24,12 +25,18 @@ export default function AiCompletion() {
     pending && data && generateCompletion(data?.get("description") as string);
   }, [pending]);
 
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [completion]);
+
   return (
     <div className="mt-10">
       {!completion && (
         <div className="mb-6 italic">{t("todo#ai_recommendation_title")}</div>
       )}
-      <div>
+      <div ref={messagesEndRef}>
         <ReactMarkdown>{completion}</ReactMarkdown>
       </div>
     </div>
